59993e9c124997659394aafbc7edd657d7d79a0f
Adam Bisek <adam.bisek@gmail.com>
2016-06-15 11:11:56 +0200

    Workshop sandbox

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,9 +1,5 @@
 <?php
 
-// this is SAMPLE DUMMY BOOTSTRAP from an app
-// it represents bootstrap in your application/framework
-// here may be created an DI Container, services, etc.
-
 class FooService {
 
 	public function bar()
@@ -13,5 +9,7 @@ class FooService {
 
 }
 
+// create and return Nette DIC
 $container = new stdClass();
 $container->fooService = new FooService();
+return $container;
